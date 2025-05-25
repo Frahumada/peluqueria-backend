d3f4d8d6da08bd5a26969fe0176648ac947aa58e
Rodrigo Ceballos <frahumada.dev@gmail.com>
2025-05-25 15:47:04 -0300

    cambios en ruteo, probar que resuelva /api/precios

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const morgan = require("morgan");
 const pricesRouter = require("./routes/pricing.routes.js");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 // Conexión a MongoDB Atlas (incluye opciones para evitar warnings)
 mongoose.connect(process.env.MONGO_URI, {
